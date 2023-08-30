5981137c7dfb390b675f2b1c281345aa8c11812b
Giovanni Souza <GSMartins999>
2023-08-30 18:16:44 -0300

    arrumei a mensagem

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -6,68 +6,81 @@
         <h6>1- Selecione o dia em que deseja fazer a refeição.</h6>
         <h6>2- No box que aparecerá selecione as rejeições desejadas, confirme o dia e aperte em 'Agendar'.</h6>
     </div>
-        <div class="py-1 pl-8 pr-8" id="calendar"></div>
 
-        <div class="modal fade" id="modalCreate" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div>
+            @if (session()->has('message'))
+            <div class="p-3 bg-green-300 text-green-700 rounded shadow-sm" id="alert">
+                {{ session('message') }}
+            </div>
+
+            @endif
+        </div>
+    <div class="py-1 pl-8 pr-8" id="calendar"></div>
+
+    <div class="modal fade" id="modalCreate" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-            <div class="modal-body">
-                <h5 class="modal-title" id="exampleModalLabel">Agendar refeição</h5>
-                  @error('Reserva')<span class="text-red-500 text/xs">{{$message}} </span> @enderror
+                <div class="modal-body">
+                    <h5 class="modal-title" id="exampleModalLabel">Agendar refeição</h5>
+                    @error('Reserva')<span class="text-red-500 text/xs">{{$message}} </span> @enderror
 
-                      <div>
-                            @if (session()->has('message'))
-                                <div class="p-3 bg-green-300 text-green-700 rounded shadow-sm ">
-                                    {{ session('message') }}
-                                </div>
-                            @endif
+
+
+                    <form wire:submit.prevent='store' method="post" action="" id="cadRef">
+                        <div class="form-group">
+                            <label for="almoco" class="col-form-label">Almoço:</label>
+                            <input type="checkbox" name='refeicao' wire:model="almoco" class="refeicao" value="almoco">
                         </div>
-                  
-                <form wire:submit.prevent='store' method="post" action="" id="cadRef">
-                <div class="form-group">
-                    <label for="almoco" class="col-form-label">Almoço:</label>
-                    <input type="checkbox" name= 'refeicao' wire:model="almoco" class="refeicao"  value="almoco">
-                </div>
-                 <div class="form-group">
-                    <label for="janta" class="col-form-label">Janta:</label>
-                    <input type="checkbox" name= 'refeicao'  wire:model="janta" class="refeicao" value="janta">
-                </div>
-                  <div class="form-group">
-                    <label for="start" class="col-form-label">Dia:</label>
-                    <input type="date" name= 'start' wire:model.defer="start"  class="refeicao" id="start">
-                </div>
+                        <div class="form-group">
+                            <label for="janta" class="col-form-label">Janta:</label>
+                            <input type="checkbox" name='refeicao' wire:model="janta" class="refeicao" value="janta">
+                        </div>
+                        <div class="form-group">
+                            <label for="start" class="col-form-label">Dia:</label>
+                            <input type="date" name='start' wire:model.defer="start" class="refeicao" id="start">
+                        </div>
 
-                <button type="submit" id="registerBtn" class="btn btn-primary">Agendar</button>
-                <button class="btn btn-secondary">Cancelar</button>
-                </form>
-            </div>
+                        <button type="submit" id="registerBtn" class="btn btn-primary">Agendar</button>
+                        <button class="btn btn-secondary">Cancelar</button>
+                    </form>
+                </div>
             </div>
         </div>
 
     </div>
 
-<div>
-    @push('script')
+    <div>
+        @push('script')
         <script>
-                $('#modalCreate').on('hiden.bs.modal', function(){
-                    @this.almoco= '';
-                    @this.janta= '';
-                    @this.start= '';
-                 })
-                const calendarEl = document.getElementById('calendar');
-                const calendar = new FullCalendar.Calendar(calendarEl, {
+            $('#modalCreate').on('hiden.bs.modal', function() {
+                @this.almoco = '';
+                @this.janta = '';
+                @this.start = '';
+            })
+            const calendarEl = document.getElementById('calendar');
+            const calendar = new FullCalendar.Calendar(calendarEl, {
                 weekends: false,
                 initialView: 'dayGridMonth',
                 locale: 'pt-BR',
                 timeZone: 'America/Sao_Paulo',
                 selectable: true,
-                select: function({startStr}){
+                select: function({
+                    startStr
+                }) {
                     @this.start = startStr;
                     $('#modalCreate').modal('toggle')
-                    }
-                });
-                calendar.render();
+                }
+            });
+            calendar.render();
         </script>
-    @endpush
 
-</div>
+        <script>
+             setTimeout(function()
+                {
+                    $("#alert").remove();
+                },2000
+            );
+        </script>
+        @endpush
+
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\{
     Calendar, CardapioController
 };
+use Livewire\Livewire;
 
 /*
 |--------------------------------------------------------------------------
